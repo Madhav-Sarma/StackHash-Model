40d52861817ef8a970f5f35faeb52b155a0761ec
Madhav-Sarma <a.madhavsarma@gmail.com>
2024-09-01 22:09:50 +0530

    build

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './components/redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 if (process.env.NODE_ENV==='production') disableReactDevTools()
 ReactDOM.render(
